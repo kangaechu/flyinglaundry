c7baf9fa9ce6ce536d71771f76caed120682b0d4
Kangaechu <kangae2@gmail.com>
2019-12-12 21:59:16 +0900

    現在時刻から24時間分の風速・風向を使用するよう修正

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from tenki import Tenki
+from datetime import datetime
 
 if __name__ == '__main__':
     url = 'https://tenki.jp/forecast/1/2/1400/1100/1hour.html'
     tenki = Tenki(url)
+
+    current_hour = datetime.now().hour
+    wind_speed_24h = tenki.wind_speed()[current_hour:current_hour + 24]
+    wind_blow_24h = tenki.wind_blow()[current_hour:current_hour + 24]
+
     flying_laundry = False
-    for wind_speed, wind_blow in zip(tenki.wind_speed(), tenki.wind_blow()):
+    for wind_speed, wind_blow in zip(wind_speed_24h, tenki.wind_blow()):
         # 北風で風速5m以上だと洗濯物が吹っ飛ぶ
         if '北' in wind_blow and int(wind_speed) >= 5:
             flying_laundry = True
     if flying_laundry:
         print("洗濯物ふっとび注意")
-
